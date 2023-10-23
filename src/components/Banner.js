7aa6dcc6cad7854204396a22ad10e54a9c7c556f
Beyondtarun <teg.tarun@gmail.com>
2023-10-23 21:33:23 +0530

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,7 @@ import g20 from "../images/g20.jpg";
 import aicte from "../images/aicte.png";
 import university from "../images/univeristy.png";
 import clglogo from "../images/clg_logo1.png";
+import iic from "../images/iic.png";
 // components
 import Footer from "./Footer";
 import ContactUs from "./ContactUs";
@@ -16,40 +17,42 @@ import Club from "../components2/Club";
 import ClubLeaders from "../components2/ClubLeaders";
 // this file contain all the components
 
-function BannerTop(){
-  return(
-<>
-<div class="topdiv flex  justify-between items-center bgwhite ">
-          <div>
-            <img className="ggsipu" src={university} alt="GGSIPU"></img>
-          </div>
-          <div>
-            <img className="clglogo" src={clglogo} alt="BCIIT"></img>
-          </div>
-          <div>
-            <img className="headerimg" src={g20} alt="G20"></img>
-          </div>
-          <div>
-            <img className="aicte" src={aicte} alt="AICTE"></img>
-          </div>
+function BannerTop() {
+  return (
+    <>
+      <div class="topdiv flex  justify-between items-center bgwhite ">
+        <div>
+          <img className="clglogo" src={clglogo} alt="BCIIT"></img>
         </div>
+        <div>
+          <img className="ggsipu" src={university} alt="GGSIPU"></img>
+        </div>
+        <div>
+          <img className="headerimg" src={g20} alt="G20"></img>
+        </div>
+        <div>
+          <img className="iic" src={iic} alt="AICTE"></img>
+        </div>
+        <div>
+          <img className="aicte" src={aicte} alt="AICTE"></img>
+        </div>
+       
+      </div>
 
-        <div class="clgtitle justify-center flex-col family">
-          <div class=" kk mar-left font-bold flex items-center pad1">
-            <a href="https://www.bciit.ac.in/" target="new">
-              BANARSIDAS CHANDIWALA INSTITUTE OF INFORMATION TECHNOLOGY
-            </a>
-          </div>
-
-          <div class=" mar-left font-bold justify-center flex items-center pad1 pad3">
+      <div class="clgtitle justify-center flex-col family">
+        <div class=" kk mar-left font-bold flex items-center pad1">
           <a href="https://www.bciit.ac.in/" target="new">
-          बनारसीदास चांदीवाला सूचना प्रौद्योगिकी संस्थान
-            </a>
-            
-          </div>
+            BANARSIDAS CHANDIWALA INSTITUTE OF INFORMATION TECHNOLOGY
+          </a>
         </div>
-</>
 
+        <div class=" mar-left font-bold justify-center flex items-center pad1 pad3">
+          <a href="https://www.bciit.ac.in/" target="new">
+            बनारसीदास चांदीवाला सूचना प्रौद्योगिकी संस्थान
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 function Banner() {
@@ -63,12 +66,17 @@ function Banner() {
 
         <div className=" container flex  justify-center items-center">
           <div className="subtitle family flex-col text-center white">
-            <hr/>
-            <span>THECOAD</span><br/>
-            <span className="dancing tagline">The Home of Experts in Coading Operations Alogoritms and Development.</span><hr/>
+            <hr />
+            <span>THECOAD</span>
+            <br />
+            <span className="dancing tagline">
+              The Home of Experts in Coading Operations Alogoritms and
+              Development.
+            </span>
+            <hr />
 
             {/* <h1>COAD &#123; n log n &#125;</h1> */}
-          
+
             <p className="tagline2">
               "COAD is the Programming society of BCIIT.
               <br /> Our Aim is to foster technical knowledge among students and
@@ -86,8 +94,7 @@ function Banner() {
       <div id="team">
         <Mentor />
         <TeamAnimated />
-        <Club/>{" "}
-        <ClubLeaders />
+        <Club /> <ClubLeaders />
       </div>
       <div id="ContactUs">
         <ContactUs />{" "}
@@ -98,5 +105,5 @@ function Banner() {
   );
 }
 
-export {BannerTop};
+export { BannerTop };
 export default Banner;
